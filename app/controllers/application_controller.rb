d099eb4c659e3aaced7a7aa1580212286c2610c1
Tracie Weitzman <tlweitzman@gmail.com>
2017-03-08 22:09:56 -0800

    something stopped working with showing user answers and comparison answers... ugh. more fancy buttons with css though

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,12 @@ class ApplicationController < ActionController::Base
     otheru = User.where(email: current_user.allowed_user_viewers.last.user_email)
     othera = otheru.first.answers.where(question_id: question.id)
     if (cua.first.response1 == true) && (othera.first.response1 == true)
-      puts 1
       "We both agree"
     elsif (cua.first.response2 == true) && (othera.first.response2 == true)
-      puts 2
       "We both disagree"
     elsif (cua.first.response3 == true) && (othera.first.response3 == true)
-      puts 3
       "We're both not sure"
     else
-      puts 4
       "We don't see eye to eye on this issue"
     end
   end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :user, optional: true
   belongs_to :question, optional: true
 
-  validates :response1, :response2, :response3, :presence => true
+  # validates :response1, :response2, :response3, :presence => true
 
   def show_true
     if self.response1
