6b9f44b3063cc6ffef5907f5e25066887ef0c8c4
Tracie Weitzman <tlweitzman@gmail.com>
2017-03-13 17:09:35 -0700

    add database seeded questions, without curly brackets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+question1 = Question.create(content: "I like my neighbors")
+question2 = Question.create(content: "I believe in democracy")
+question3 = Question.create(content: "I'm worried for the next generation")
+question4 = Question.create(content: "Kids deserve the best chance for their future")
+question5 = Question.create(content: "Women should be safe from domestic violence")
+question6 = Question.create(content: "Everyone should be free to express themselves as they wish")
+question7 = Question.create(content: "Clean water and air are important for our health")
+question8 = Question.create(content: "Everyone deservces the opportunity to work hard at a job that fits their abilities")
+question9 = Question.create(content: "America is a nation of immigrants")
+question10 = Question.create(content: "It's worth investigating alternative energy sources to work towards energy independence")
