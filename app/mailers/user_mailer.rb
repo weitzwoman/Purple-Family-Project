cf762a331ef365fb58f78e0d047e1a43ea9d5236
Tracie Weitzman <tlweitzman@gmail.com>
2017-03-14 14:56:07 -0700

    add welcome email view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'tlweitzman@gmail.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'https://purple-family.herokuapp.com/users/sign_in'
+    mail(to: @user.email, subject: 'Welcome to Purple Family Project')
+  end
 end
