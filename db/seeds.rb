fad15e0e4e78fa51aeda821f4b5a008447aa5e26
Tracie Weitzman <tlweitzman@gmail.com>
2017-03-20 17:03:40 -0700

    fix typo in database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ question4 = Question.create(content: "Kids deserve the best chance for their fut
 question5 = Question.create(content: "Women should be safe from domestic violence")
 question6 = Question.create(content: "Everyone should be free to express themselves as they wish")
 question7 = Question.create(content: "Clean water and air are important for our health")
-question8 = Question.create(content: "Everyone deservces the opportunity to work hard at a job that fits their abilities")
+question8 = Question.create(content: "Everyone deserves the opportunity to work hard at a job that fits their abilities")
 question9 = Question.create(content: "America is a nation of immigrants")
 question10 = Question.create(content: "It's worth investigating alternative energy sources to work towards energy independence")
